4c318e897a785c70ca52fbcb7fc066353805bedb
Pranav Rajan <rajan.pranav320@gmail.com>
2022-01-16 15:21:49 -0700

    Set up make file, main.c and assignment1.c for writing code

---
+++ assignment1/assignment1.c
@@ -0,0 +1,7 @@
+/*
+Operating Systems Assignment 1 Part A.
+Author: Pranav Rajan u1136324@utah.edu
+Version: January 16, 2022
+*/
+
+// import 
---
+++ assignment1/main.c
@@ -0,0 +1,8 @@
+#include "assignment1.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, world");
+    return 0;
+}
