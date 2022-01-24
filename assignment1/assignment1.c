d3a00f9f273d48d428cf602dcbf9d71f5b001055
Pranav Rajan <rajan.pranav320@gmail.com>
2022-01-24 13:54:33 -0700

    fix increment

--- assignment1/assignment1.c
+++ assignment1/assignment1.c
@@ -158,7 +158,7 @@ void convert(enum format_t mode, uint64_t value)
     case BIN:
         printf("BIN MODE\n");
         int j;
-        for (j = 63; j >= 0; ++j)
+        for (j = 63; j >= 0; --j)
         {
             // check individual bits
             switch (value >> (1 * j) & 0X1)
