a811b88d793366ab517969cbda8dab365f3a286d
Pranav Rajan <rajan.pranav320@gmail.com>
2022-01-25 14:40:42 -0700

    fix

--- assignment1/assignment1.c
+++ assignment1/assignment1.c
@@ -245,7 +245,7 @@ struct elt *str_to_list(const char *str)
         if (new_elt == NULL)
         {
             struct elt *start_elt = head;
-            struct elt *next_elt;
+            struct elt *next_elt = NULL;
             while (start_elt != NULL)
             {
                 next_elt = start_elt->link;
