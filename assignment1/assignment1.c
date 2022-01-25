a2ab51758b8e7ee2e6f26c47d7afe615ec64eb16
Pranav Rajan <rajan.pranav320@gmail.com>
2022-01-25 11:43:50 -0700

    find a bug

--- assignment1/assignment1.c
+++ assignment1/assignment1.c
@@ -231,7 +231,7 @@ Allocate and return a pointer to a linked list of struct elts.
 struct elt *str_to_list(const char *str)
 {
     // find size of char array
-    int size = sizeof(*str) / sizeof(char);
+    int size = sizeof(str) / sizeof(char);
     printf("length of string: %d", size);
 
     // declare a head node
