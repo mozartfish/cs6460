3ca4e803d24020951c40f9d673fca487d26dcd69
Pranav Rajan <rajan.pranav320@gmail.com>
2022-01-24 21:08:54 -0700

    Rewrite tests

--- assignment1/main.c
+++ assignment1/main.c
@@ -4,21 +4,23 @@
 int main()
 {
     printf("Byte Sort\n");
-    printf("original value passed in: %lx\n", 0x0403deadbeef0201);
+    printf("Input: %lx\n", 0x0403deadbeef0201);
     uint64_t foo = byte_sort(0x0403deadbeef0201);
-    printf("value of foo is: %lx\n", foo);
+    printf("Expected Result: %lx\n", 0xefdebead04030201);
+    printf("Actual Result: %lx\n", foo);
 
     printf("Nibble Sort\n");
-    printf("original value passed in: %lx\n", 0x0403deadbeef0201);
+    printf("Input: %lx\n", 0x0403deadbeef0201);
     uint64_t bar = nibble_sort(0x0403deadbeef0201);
-    printf("value of bar is: %lx\n", bar);
+    printf("Expected Result: %lx\n", 0xfeeeddba43210000);
+    printf("Actual Result: %lx\n", bar);
 
     printf("Convert\n");
-    printf("original value passed in: %x\n", 0Xdeadbeef);
-    convert(HEX, 0Xdeadbeef);
+    printf("Input: %x\n", 0Xdeadbeef);
+    printf("Expected Hex Result: %lx", 0x00000000deadbeef);
+    convert(HEX, 0xdeadbeef);
     printf("Binary value\n");
-    convert(BIN, 0Xdeadbeef);
+    convert(BIN, 0xdeadbeef);
     printf("Oct value\n");
-    convert(OCT, 0Xdeadbeef);
-    printf("hex result should be: 00000000deadbeef\n");
+    convert(OCT, 0xdeadbeef);
 }
