4a84ce2e1fe8bd8549d9af7ecf9ac26134e10b1b
Pranav Rajan <rajan.pranav320@gmail.com>
2022-01-26 12:51:25 -0700

    drew the U for the draw U function

---
+++ draw.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    printf("-----                       -----");
+    printf("\n");
+    printf("|   |                       |   |");
+    printf("\n");
+    printf("|   |                       |   |");
+    printf("\n");
+    printf("|   |                       |   |");
+    printf("\n");
+    printf("|   |                       |   |");
+    printf("\n");
+    printf("|   |                       |   |");
+    printf("\n");
+    printf("|   |                       |   |");
+    printf("\n");
+    printf("|   |                       |   |");
+    printf("\n");
+    printf("|   | _ _ _ _ _ _ _ _ _ _ _ |   |");
+    printf("\n");
+    printf("\\ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _/");
+    printf("\n");
+    return 0;
+    }
