341a16fc40aa9803310084277c17e168ce340239
Pranav Rajan <rajan.pranav320@gmail.com>
2022-03-23 17:37:44 -0600

    Add in assignment 4

--- draw.c
+++ draw.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("-----                       -----");
-    printf("\n");
-    printf("|   |                       |   |");
-    printf("\n");
-    printf("|   |                       |   |");
-    printf("\n");
-    printf("|   |                       |   |");
-    printf("\n");
-    printf("|   |                       |   |");
-    printf("\n");
-    printf("|   |                       |   |");
-    printf("\n");
-    printf("|   |                       |   |");
-    printf("\n");
-    printf("|   |                       |   |");
-    printf("\n");
-    printf("|   | _ _ _ _ _ _ _ _ _ _ _ |   |");
-    printf("\n");
-    printf("\\ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _/");
-    printf("\n");
+    printf("-----                       -----\n"); // 35 bytes 1 
+    printf("|   |                       |   |\n"); // 4 bytes 2
+    printf("|   |                       |   |\n"); // 33 bytes 3
+    printf("|   |                       |   |\n"); // 4
+    printf("|   |                       |   |\n"); // 5
+    printf("|   |                       |   |\n"); // 6
+    printf("|   |                       |   |\n"); // 7
+    printf("|   |                       |   |\n"); // 8
+    printf("|   | _ _ _ _ _ _ _ _ _ _ _ |   |\n"); // 9
+    printf("\\ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _/\n"); // 10
     return 0;
     }
+
---
+++ foo.c
@@ -0,0 +1,12 @@
+int main(){
+    printf("hello, world!");
+}
+
+void (char *foobar){
+    printf("this is a test");
+}
+
+int thing(int d)
+{
+    printf("this is a function in c");
+}
